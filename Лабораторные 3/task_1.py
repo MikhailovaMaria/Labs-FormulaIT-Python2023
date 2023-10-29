3652da04b70f8841b2534d3fbd1e374ae51f04c6
MikhailovaMaria <147265187+MikhailovaMaria@users.noreply.github.com>
2023-10-29 20:32:49 +0300

    Add files via upload

---
+++ Лабораторные 3/task_1.py
@@ -0,0 +1,16 @@
+def find_item_index(item_list, search_item):
+    if search_item in item_list:
+        index = item_list.index(search_item)
+        return index
+    else:
+        return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_item_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
